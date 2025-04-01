5e08f54e49425d5093ea8a0503d9b1efa986fc60
Chandrakana Nandi <chandra.nandi90@gmail.com>
2025-04-01 16:22:58 -0700

    chore: change var name to address

--- cvlr-soroban/src/log.rs
+++ cvlr-soroban/src/log.rs
@@ -3,6 +3,6 @@ use soroban_sdk::Address;
 
 pub fn clog_address(addr: &Address) {
     // lower 8 bits is tag (77), we want the upper 56 bits which is a u64
-    let payload = addr.to_val().get_payload() >> 8;
-    clog!(payload);
+    let address = addr.to_val().get_payload() >> 8;
+    clog!(address);
 }
