2203a5d798cb7121d4ec3854845e658cee7a45fc
Chandrakana Nandi <chandra.nandi90@gmail.com>
2025-04-01 16:13:44 -0700

    feat: logging for address type

--- cvlr-soroban/src/lib.rs
+++ cvlr-soroban/src/lib.rs
@@ -1,8 +1,10 @@
 #![no_std]
 mod auth;
 mod nondet;
+mod log;
 
 pub use auth::*;
 pub use nondet::*;
+pub use log::*;
 
 pub mod testutils {}
---
+++ cvlr-soroban/src/log.rs
@@ -0,0 +1,8 @@
+use cvlr_log::clog;
+use soroban_sdk::Address;
+
+pub fn clog_address(addr: &Address) {
+    // lower 8 bits is tag (77), we want the upper 56 bits which is a u64
+    let payload = addr.to_val().get_payload() >> 8;
+    clog!(payload);
+}
