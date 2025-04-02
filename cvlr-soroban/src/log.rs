4e80fac45768269cfab2dc47b26ea9b902958521
Chandrakana Nandi <chandra.nandi90@gmail.com>
2025-04-01 19:11:01 -0700

    chore: reimplement with cvlr_log

--- cvlr-soroban/src/log.rs
+++ cvlr-soroban/src/log.rs
@@ -1,8 +1,10 @@
-use cvlr_log::clog;
-use soroban_sdk::Address;
+pub struct Addr<'a>(pub &'a soroban_sdk::Address);
 
-pub fn clog_address(addr: &Address) {
-    // lower 8 bits is tag (77), we want the upper 56 bits which is a u64
-    let address = addr.to_val().get_payload() >> 8;
-    clog!(address);
+impl cvlr_log::CvlrLog for Addr<'_> {
+    #[inline(always)]
+    fn log(&self, tag: &str, logger: &mut cvlr_log::CvlrLogger) {
+        // lower 8 bits is tag (77), we want the upper 56 bits which is a u64
+        let v = self.0.to_val().get_payload() >> 8;
+        logger.log_u64(tag, v);
+    }
 }
