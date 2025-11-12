d35d5dcd67c21fcd20429f6d49a56fcfe90b5385
Chandrakana Nandi <chandra.nandi90@gmail.com>
2025-11-12 15:27:29 -0800

    mock contractevent and topic

--- cvlr-soroban-derive/src/lib.rs
+++ cvlr-soroban-derive/src/lib.rs
@@ -17,3 +17,40 @@ pub fn declare_rule(input: TokenStream) -> TokenStream {
 pub fn rule(attr: TokenStream, input: TokenStream) -> TokenStream {
     rule::rule(attr, input)
 }
+
+// A dummy field attribute to return the original input.
+#[proc_macro_attribute]
+pub fn topic(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    item
+}
+
+// A dummy macro for `#[contractevent]`.
+// It removes `#[topic]` and returns the struct without any changes.
+#[proc_macro_attribute]
+pub fn contractevent(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(item as syn::ItemStruct);
+    let mut output = input.clone();
+
+    // Removes #[topic] attributes from fields
+    if let syn::Fields::Named(ref mut fields) = output.fields {
+        for field in &mut fields.named {
+            field.attrs = field
+                .attrs
+                .iter()
+                .filter(|a| !a.path().is_ident("topic"))
+                .cloned()
+                .collect::<Vec<syn::Attribute>>();
+        }
+    }
+
+    let vis = &output.vis;
+    let ident = &output.ident;
+    let fields = &output.fields;
+
+    // Return the struct without `#[topic]`
+    let expanded = quote::quote! {
+        #vis struct #ident #fields
+    };
+
+    expanded.into()
+}
